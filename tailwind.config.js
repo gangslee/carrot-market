7238bbf7aff50ef2c03895a6496c4dc61d80ee8b
gangslee <grandnet1225@gmail.com>
2022-02-04 02:11:13 +0900

    TailwindCSS Setup

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"], // tailwind 사용하는 Component or Page
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
